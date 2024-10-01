1b659a9f05555aa98fb2f9cb05d7797cafa90d97
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-01 15:05:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ scaler  = MinMaxScaler(feature_range=(0,1))
 
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
-prediction_days = 60
+prediction_days = 90
 
 x_train = []
 y_train = []
